aeb0540dcdf144368800ba23375d2be7d9a0b9d8
ltaoj <625871322@qq.com>
2017-08-07 17:23:11 +0800

    WithdrawInstDAO

--- src/main/java/org/csu/sm/persistence/WithdrawInstDAO.java
+++ src/main/java/org/csu/sm/persistence/WithdrawInstDAO.java
@@ -1,7 +1,33 @@
 package org.csu.sm.persistence;
 
+import org.csu.sm.domain.WithdrawInst;
+
+import javax.persistence.PersistenceException;
+import java.util.List;
+
 /**
  * Created by ltaoj on 2017/8/7.
  */
 public interface WithdrawInstDAO {
+    /**
+     * 添加退队入队说明
+     * @param withdrawInst
+     * @throws PersistenceException
+     */
+    void insertWithdrawInst(WithdrawInst withdrawInst) throws PersistenceException;
+
+    /**
+     * 更新退队入队说明
+     * @param withdrawInst
+     * @throws PersistenceException
+     */
+    void updateWithdrawInst(WithdrawInst withdrawInst) throws PersistenceException;
+
+    /**
+     * 获取运动员退队入队说明列表
+     * @param studentId
+     * @return
+     * @throws PersistenceException
+     */
+    List<WithdrawInst> getWithdrawInst(long studentId) throws PersistenceException;
 }
--- src/main/java/org/csu/sm/persistence/impl/WithdrawDAOimpl.java
+++ src/main/java/org/csu/sm/persistence/impl/WithdrawDAOimpl.java
@@ -1,7 +1,38 @@
 package org.csu.sm.persistence.impl;
 
+import org.csu.sm.domain.WithdrawInst;
+import org.csu.sm.persistence.WithdrawInstDAO;
+import org.csu.sm.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.PersistenceException;
+import java.util.List;
+
 /**
  * Created by ltaoj on 2017/8/7.
  */
-public class WithdrawDAOimpl {
+public class WithdrawDAOimpl implements WithdrawInstDAO{
+    public void insertWithdrawInst(WithdrawInst withdrawInst) throws PersistenceException {
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(withdrawInst);
+        transaction.commit();
+    }
+
+    public void updateWithdrawInst(WithdrawInst withdrawInst) throws PersistenceException {
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(withdrawInst);
+        transaction.commit();
+    }
+
+    public List<WithdrawInst> getWithdrawInst(long studentId) throws PersistenceException {
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = "from WithdrawInst as w where studentId=" + studentId;
+        List<WithdrawInst> list = session.createQuery(hql).list();
+        transaction.commit();
+        return list;
+    }
 }
