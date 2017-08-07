252057e6c8b1f7eacdaee2b463a95b84a0b43511
ltaoj <625871322@qq.com>
2017-08-08 00:28:45 +0800

    AccountServiceimpl实现Student studentLogin(Signon signon)、void changeStudPassword(long studentId, String newPassword, String oldPassword)、Teacher teacherLogin(Teacher teacher)、void changeTeacPassword(String username, String newPassword, String oldPassword)

--- src/main/java/org/csu/sm/service/AccountService.java
+++ src/main/java/org/csu/sm/service/AccountService.java
@@ -1,7 +1,46 @@
 package org.csu.sm.service;
 
+import org.csu.sm.domain.Signon;
+import org.csu.sm.domain.Student;
+import org.csu.sm.domain.Teacher;
+import org.csu.sm.exception.service.AccountServiceException;
+
 /**
  * Created by ltaoj on 2017/8/7.
  */
 public interface AccountService {
+
+    /**
+     * 学生登录
+     * @param signon
+     * @return
+     * @throws AccountServiceException
+     */
+    Student studentLogin(Signon signon) throws AccountServiceException;
+
+    /**
+     * 修改学生密码
+     * @param studentId
+     * @param newPassword 新密码
+     * @param oldPassword 旧密码
+     * @throws AccountServiceException
+     */
+    void changeStudPassword(long studentId, String newPassword, String oldPassword) throws AccountServiceException;
+
+    /**
+     * 老师登录
+     * @param teacher
+     * @return
+     * @throws AccountServiceException
+     */
+    Teacher teacherLogin(Teacher teacher) throws AccountServiceException;
+
+    /**
+     * 修改教师密码
+     * @param username
+     * @param newPassword
+     * @param oldPassword
+     * @throws AccountServiceException
+     */
+    void changeTeacPassword(String username, String newPassword, String oldPassword) throws AccountServiceException;
 }
--- src/main/java/org/csu/sm/service/impl/AccountServiceimpl.java
+++ src/main/java/org/csu/sm/service/impl/AccountServiceimpl.java
@@ -1,7 +1,47 @@
 package org.csu.sm.service.impl;
 
+import org.csu.sm.domain.Signon;
+import org.csu.sm.domain.Student;
+import org.csu.sm.domain.Teacher;
+import org.csu.sm.exception.service.AccountServiceException;
+import org.csu.sm.persistence.SignonDAO;
+import org.csu.sm.persistence.StudentDAO;
+import org.csu.sm.persistence.TeacherDAO;
+import org.csu.sm.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by ltaoj on 2017/8/7.
  */
-public class AccountServiceimpl {
+@Service
+public class AccountServiceimpl implements AccountService{
+
+    private SignonDAO signonDAO;
+    private StudentDAO studentDAO;
+
+    @Autowired
+    public AccountServiceimpl(SignonDAO signonDAO, StudentDAO studentDAO) {
+        this.signonDAO = signonDAO;
+        this.studentDAO = studentDAO;
+    }
+
+    public Student studentLogin(Signon signon) throws AccountServiceException {
+        Integer isLoginSuccess = signonDAO.studentLogin(signon);
+        return isLoginSuccess == 1 ? studentDAO.getStudent(signon.getStudentId()) : null;
+    }
+
+    public void changeStudPassword(long studentId, String newPassword, String oldPassword) throws AccountServiceException {
+        Integer isPasswordCorrect = signonDAO.studentLogin(new Signon(studentId, oldPassword));
+        if (isPasswordCorrect == 1) signonDAO.updateStudentSignon(new Signon(studentId, newPassword));
+    }
+
+    public Teacher teacherLogin(Teacher teacher) throws AccountServiceException {
+        return signonDAO.teacherLogin(teacher);
+    }
+
+    public void changeTeacPassword(String username, String newPassword, String oldPassword) throws AccountServiceException {
+        Teacher teacher = signonDAO.teacherLogin(new Teacher(username, oldPassword));
+        if (teacher  != null) signonDAO.updateTeacherSignon(username, newPassword);
+    }
 }
