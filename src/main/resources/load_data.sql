891fbd00cee9fb57933c2f0f98749b3d740e04c9
ltaoj <625871322@qq.com>
2017-08-09 22:57:52 +0800

    load_data.sql

---
+++ src/main/resources/load_data.sql
@@ -0,0 +1,9 @@
+### student ###
+INSERT INTO student(student_id, name, sex, student_type, verify_state) VALUES(3903150326, '李涛江', 1, 0, 0);
+INSERT INTO student(student_id, name, sex, student_type, verify_state) VALUES(3903150332, '葛凡', 1, 0, 0);
+INSERT INTO student(student_id, name, sex, student_type, verify_state) VALUES(3903150327, '陈铭明', 1, 0, 0);
+
+### signon ###
+INSERT INTO signon(student_id, password) VALUE(3903150326,  '3903150326');
+INSERT INTO signon(student_id, password) VALUE(3903150332,  '3903150332');
+INSERT INTO signon(student_id, password) VALUE(3903150327,  '3903150327');
