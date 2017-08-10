bfd0c03753a8aec132fd4c297d3ce6b6898d1688
ltaoj <625871322@qq.com>
2017-08-10 23:07:10 +0800

    modal.js使用方法：1.在html中添加模态框div。 <div class='modal fade' id='addModal' tabindex='-1' role='dialog' aria-labelledby='addModalLabel' aria-hidden='true'></div>'  2.在js文件中初始化模态框.append(model('添加新成员', 3, getModelForm(0), function () {console.log('1234');})[0]);即可完成模态框的初始化。

--- src/main/webapp/js/widge/model.js
+++ src/main/webapp/js/widge/model.js
@@ -1,20 +1,76 @@
-function model() {
+/**
+ * 模态框的样式在这里定义
+ * @type {[*]}
+ */
+var modelForms = [[{label: '姓名:', id: 'parentName', type: 'text', placeholder: '请输入家长姓名'},
+    {label: '联系方式:', id: 'parentPhone', type: 'text', placeholder: '请输入家长联系方式'},
+    {label: '关系:', id: 'parentRelation', type: 'text', placeholder: '请输入与你的关系'}],];
+
+/**
+ * 通过传入下标值获取模态框样式
+ * @param index
+ * @returns {*}
+ */
+function getModelForm(index) {
+    return modelForms[index];
+}
+
+/**
+ * 保存确认按钮函数
+ */
+var confirmEvent = function () {
     
+};
+
+/**
+ * 赋值
+ * @param feedback
+ */
+function setConfirmEvent(feedback) {
+    confirmEvent = feedback;
+}
+
+/**
+ * 执行确认函数
+ */
+function feedback() {
+    confirmEvent();
+}
+/**
+ *
+ * @param title 模态框标题
+ * @param rowNum 模态框表单行数
+ * @param formArray 模态框表单每行表单组件类型包括label, id, type, placeholder,
+ * @param feedback 当点击确定按钮时执行的函数
+ * @param values 各个表单组件的初始值,可默认为空
+ * @returns {*}
+ */
+function model(title, rowNum, rowObjArray, feedback, values) {
     function createModel() {
+
+        if (feedback != undefined) {
+            setConfirmEvent(feedback);
+        }
+
+        if (values == undefined){
+            values = [];
+            for (var i = 0;i < rowNum;i++) {
+                values[i] = "";
+            }
+        }
+
         // var model = createElementWithAttribute("div",{class:'modal fade', id:'myModal', tabindex:-1, role:'dialog', 'aria-labelledby':'myModalLabel', 'aria-hidden':true});
         var dialog = createElementWithAttribute("div", {class: 'modal-dialog'});
         var content = createElementWithAttribute("div", {class: 'modal-content'});
 
         var header = createElementWithAttribute("div", {class: "modal-header"});
         header.appendChild(createElementWithHTML(createElementWithAttribute("button", {class: 'close', type: 'button', 'data-dismiss': 'modal', 'aria-hidden': true}), '&times;'));
-        header.appendChild(createElementWithHTML(createElementWithAttribute("h4", {class:'modal-title'}), "添加新成员"));
+        header.appendChild(createElementWithHTML(createElementWithAttribute("h4", {class:'modal-title'}), title));
         var body = createElementWithAttribute("div", {class: "modal-body"});
-        body.appendChild(createForm(3, [{label: '姓名:', id: 'parentName', type: 'text', placeholder: '请输入家长姓名'},
-            {label: '联系方式:', id: 'parentPhone', type: 'text', placeholder: '请输入家长联系方式'},
-            {label: '关系:', id: 'parentRelation', type: 'text', placeholder: '请输入与你的关系'}]));
+        body.appendChild(createForm(rowNum, rowObjArray, values));
         var footer = createElementWithAttribute("div", {class: "modal-footer"});
         footer.appendChild(createElementWithHTML(createElementWithAttribute("button", {type: 'button', class: 'btn btn-default', 'data-dismiss': 'modal'}), '关闭'));
-        footer.appendChild(createElementWithHTML(createElementWithAttribute("button", {type: 'button', class: 'btn btn-primary', id: 'confirmAdd'}), '确认添加'));
+        footer.appendChild(createElementWithHTML(createElementWithAttribute("button", {type: 'button', class: 'btn btn-primary', id: 'confirmAdd', onclick: 'feedback()'}), '确认添加'));
         content.appendChild(header);
         content.appendChild(body);
         content.appendChild(footer);
@@ -23,7 +79,7 @@ function model() {
         return [dialog, content];
     }
     
-    function createForm(rowNum, objArray) {
+    function createForm(rowNum, objArray, values) {
 
         function createFormGroup() {
             return createElementWithAttribute("div", {class: 'form-group'});
@@ -35,7 +91,7 @@ function model() {
             var formGroup = createFormGroup();
             formGroup.appendChild(createElementWithHTML(createElementWithAttribute("label", {for:objArray[i].id, class:"col-lg-2 control-label"}), objArray[i].label));
             var div = createElementWithAttribute("div", {class: "col-lg-10"});
-            div.appendChild(createElementWithAttribute("input", {type:objArray[i].type, class:"form-control", id:objArray[i].id, placeholder:objArray[i].placeholder}));
+            div.appendChild(createElementWithAttribute("input", {type:objArray[i].type, class:"form-control", id:objArray[i].id, placeholder:objArray[i].placeholder, value: values[i]}));
             formGroup.appendChild(div);
             form.appendChild(formGroup);
         }
