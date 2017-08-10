0e0fbe9f305d2b5162670a015e2bd47e3d0f4029
ltaoj <625871322@qq.com>
2017-08-10 09:37:19 +0800

    alert.js 用法:方法alert(type, text)返回一个div,type为success、info、warning、danger,

--- src/main/webapp/js/widge/alert.js
+++ src/main/webapp/js/widge/alert.js
@@ -1,29 +1,61 @@
-function alertSuccess() {
-    var alert = document.createElement("div");
-    alert.setAttribute("class", "alert alert-success");
-    alert.appendChild(getTagi());
+function alert(type, text) {
+
+    function createAlert(type) {
+        var alert = document.createElement("div");
+        alert.setAttribute("class", getStyle(type)[0]);
+        return alert;
+    }
+
+    function getTagi(type) {
+        var i = document.createElement("i");
+        i.setAttribute("class", getStyle(type)[1]);
+        return i;
+    }
+
+    function getTaga() {
+        var a = document.createElement("a");
+        a.setAttribute("class", "close fade");
+        a.setAttribute("href", "#");
+        a.setAttribute("data-dismiss", "alert");
+        a.innerHTML = "&times";
+        return a;
+    }
+
+    function getTagStrong(text) {
+        var strong = document.createElement("strong");
+        strong.innerHTML = text;
+        return strong;
+    }
+
+    function getStyle(type) {
+        var alertStyle = "";
+        var iconStyle = "";
+        switch (type) {
+            case "success":
+                alertStyle = "alert alert-success";
+                iconStyle = "icon-ok-sign";
+                break;
+            case "info":
+                alertStyle = "alert alert-info";
+                iconStyle = "icon-exclamation-sign";
+                break;
+            case "warning":
+                alertStyle = "alert alert-warning";
+                iconStyle = "icon-warning-sign";
+                break;
+            case "danger":
+                alertStyle = "alert alert-danger";
+                iconStyle = "icon-remove-sign";
+                break;
+        }
+        return [alertStyle, iconStyle];
+    }
+    if (type == undefined) type = "success";
+    if (text == undefined) text = "操作成功!"
+
+    var alert = createAlert(type);
+    alert.appendChild(getTagi(type));
     alert.appendChild(getTaga());
-    alert.appendChild(getTagStrong());
+    alert.appendChild(getTagStrong(text));
     return alert;
-}
-
-function getTagi() {
-    var i = document.createElement("i");
-    i.setAttribute("class", "icon-ok-sign");
-    return i;
-}
-
-function getTaga() {
-    var a = document.createElement("a");
-    a.setAttribute("class", "close fade");
-    a.setAttribute("href", "#");
-    a.setAttribute("data-dismiss", "alert");
-    a.innerHTML = "&times";
-    return a;
-}
-
-function getTagStrong() {
-    var strong = document.createElement("strong");
-    strong.innerHTML = "成功!";
-    return strong;
 }
