5fefeaeb8010a2724c6e719e0358333f5edfc4d4
ltaoj <625871322@qq.com>
2017-08-14 18:52:17 +0800

    修改spring security配置.headers().frameOptions().sameOrigin()

--- src/main/java/org/csu/sm/config/SecurityConfig.java
+++ src/main/java/org/csu/sm/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and().formLogin().loginPage("/signin")
                 .and().logout().logoutUrl("/logout").logoutSuccessUrl("/signin").invalidateHttpSession(true)
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .headers().frameOptions().sameOrigin();
     }
 }
